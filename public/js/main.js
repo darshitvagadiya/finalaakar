0139e6908aa92b48c6e2e47d32e20ab257e315a1
darshitvagadiya <darshitvagadiya@yahoo.com>
2019-11-24 23:15:42 +0530

    add

--- public/js/main.js
+++ public/js/main.js
@@ -276,6 +276,29 @@
 
 	};
 
+	// var clients = function(){
+	// 				    $('.customer-logos').slick({
+	// 				        slidesToShow: 6,
+	// 				        slidesToScroll: 1,
+	// 				        autoplay: true,
+	// 				        autoplaySpeed: 1500,
+	// 				        arrows: false,
+	// 				        dots: false,
+	// 				        pauseOnHover: false,
+	// 				        responsive: [{
+	// 				            breakpoint: 768,
+	// 				            settings: {
+	// 				                slidesToShow: 4
+	// 				            }
+	// 				        }, {
+	// 				            breakpoint: 520,
+	// 				            settings: {
+	// 				                slidesToShow: 3
+	// 				            }
+	// 				        }]
+	// 				    });
+	// 				};
+
 	
 	$(function(){
 		mobileMenuOutsideClick();
@@ -290,6 +313,7 @@
 		parallax();
 		sliderMain();
 		testimonialCarousel();
+		// clients();
 	});
 
 
